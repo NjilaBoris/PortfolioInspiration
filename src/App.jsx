57157bbe08c56b77f77631d1a89526e800720863
NjilaBoris <njilaboris2@gmail.com>
2025-07-23 01:26:03 +0100

    Implement MagneticButton Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+const App = () => {
+  const container = useRef(null);
 
+  useGSAP(
+    () => {
+      // Initialize ScrollSmoother
+      ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 2.5,
+        effects: true,
+      });
+    },
+    { scope: container }
+  );
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div ref={container} id="smooth-wrapper">
+      <div id="smooth-content" className="relative"></div>
+    </div>
+  );
+};
 
-export default App
+export default App;
